8dc7b2eebe287af2de42c671a9cd3bf7c9aff4e6
Jules Torfs <julestorfs@amps.be>
2022-03-29 11:24:09 +0200

    Saving changes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+// include the library for HttpClientFactory
+
+
 
 namespace DotNetWeather // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var httpClient = new HttpClientFactory.Create();
+
+            var url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1";
+            var data = await httpClient.GetStringAsync(url);
+
+            Console.WriteLine(data);
+
             Console.WriteLine("Hello World!");
+            // do a http request to get the weather
+
         }
     }
 }
