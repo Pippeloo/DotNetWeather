96ec75f35dc9bdddd69c8fb3997ed83eba2beb6e
Jules Torfs <julestorfs@amps.be>
2022-03-29 14:51:35 +0200

    First fully working version

--- Program.cs
+++ Program.cs
@@ -11,27 +11,79 @@ namespace DotNetWeather // Note: actual namespace depends on the project name.
     internal class Program
     {
         static async Task Main(string[] args)
-        {
-            var lat = "51.16557";
-            var lon = "4.98917";
+        {   
             var api_key = "fd0f221dcbc5cce7a9d7a2f832156646";
 
-            var data = await GetWeather(lat, lon, api_key);
+            Console.WriteLine("===============================");
+            Console.WriteLine("Welcome to the weather app!");
+
+            var locationName = AskCity();
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Getting weather for {locationName}");
+
+            var data = await GetWeather(locationName, api_key);
+
+            if (data == "")
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Sorry, we couldn't find that city.");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Please try again.");
+                Console.WriteLine("===============================");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+                return;
+            }
 
             var json = JObject.Parse(data);
+            var weather = json["weather"][0]["main"].ToString();
+            var tempCelcius = json["main"]["temp"].ToString();
+            var windSpeed = json["wind"]["speed"].ToString();
+            var humidity = json["main"]["humidity"].ToString();
 
-            Console.WriteLine(json["coord"]["lon"]);
-            // do a http request to get the weather
+
+            Console.WriteLine($"The weather is {weather}");
+            Console.WriteLine($"The temperature is {tempCelcius}°C");
+            Console.WriteLine($"The wind speed is {windSpeed}m/s");
+            Console.WriteLine($"The humidity is {humidity}%");
+            Console.WriteLine("===============================");
         }
 
-        private static async Task<String> GetWeather(string lat, string lon, string api_key)
+        private static async Task<String> GetWeather(string location_name, string key)
         {
             var httpClient = new HttpClient();
 
-            var url = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}";
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={location_name}&limit=1&units=metric&appid={key}";
             var data = await httpClient.GetStringAsync(url);
 
-            return data;
+            var data_json = JObject.Parse(data);
+
+            if (data_json["cod"].ToString() == "200")
+            {
+                return data;
+            }
+            else
+            {
+                Console.WriteLine("Error");
+                return "";
+            }
+        }
+
+        private static String AskCity()
+        { 
+            while (true) {
+                Console.Write("Enter the city name: ");
+                var locationName = Console.ReadLine();
+                if (locationName == null || locationName == "")
+                {
+                    Console.WriteLine("Please enter a valid city name.");
+                }
+                else
+                {
+                    return locationName;
+                }
+            }
         }
     }
 }
